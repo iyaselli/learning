103934d5fe59f2ed39535348fbbad1884c284d08
Ignacio Yaselli <ignacio.yaselli@datalytyx.com>
2025-06-27 20:03:18 +0000

    finalising intermediate files

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,52 +1,10 @@
 with
 
-    -- Import CTEs
-    customers as ( select * from {{ ref('stg_jaffle_shop__customers') }}),
+customers as ( select * from {{ ref('stg_jaffle_shop__customers') }}),
 
-    orders as ( select * from {{ ref('stg_jaffle_shop__orders') }}),
-
-    payments as ( select * from {{ ref('stg_stripe__payments') }}),
-
-    -- Logical CTEs
-    completed_payments as (
-        select
-            order_id,
-            max(payment_created_at) as payment_finalized_date,
-            sum(payment_amount) as total_amount_paid
-        from payments
-        where payment_status <> 'fail'
-        group by 1
-    ),
-
-    customer_orders as (
-        select
-            customers.customer_id,
-            min(orders.order_placed_at) as first_order_date,
-            max(orders.order_placed_at) as most_recent_order_date,
-            count(orders.order_id) as number_of_orders
-        from customers
-        left join orders on orders.customer_id = customers.customer_id
-        group by 1
-    ),
+paid_orders as (select * from {{ ref('int_orders') }}),
 
 
-    paid_orders as (
-        select
-            orders.order_id,
-            orders.customer_id,
-            orders.order_placed_at,
-            orders.order_status,
-            completed_payments.total_amount_paid,
-            completed_payments.payment_finalized_date,
-            customers.customer_first_name,
-            customers.customer_last_name
-        from orders 
-        left join completed_payments on orders.order_id = completed_payments.order_id
-        left join customers on orders.customer_id = customers.customer_id
-    ),
-
-
-    -- Final CTEs
 final as (
     select
         paid_orders.order_id,
@@ -55,8 +13,8 @@ final as (
         paid_orders.total_amount_paid,
         paid_orders.order_status,
         paid_orders.payment_finalized_date,
-        paid_orders.customer_first_name,
-        paid_orders.customer_last_name,
+        customers.customer_first_name,
+        customers.customer_last_name,
 
     -- sales transaction sequence
         row_number() over (order by paid_orders.order_id) as transaction_seq,
@@ -89,6 +47,7 @@ final as (
         ) as fdos
 
     from paid_orders
+    left join customers on customers.customer_id = paid_orders.customer_id
 
 
     )
